6a1ba81002663fed4731a24e0238701cd61863ed
Praful Rana <prafulrana@gmail.com>
2015-05-22 14:19:04 -0700

    initial

---
+++ server/app.js
@@ -0,0 +1,34 @@
+(function() {
+    'use strict';
+
+    var express = require('express');
+    var path = require('path');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+
+    var routes = require('./routes/index');
+
+    var app = express();
+
+    app.use(bodyParser.json());
+
+    app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
+        if (req.method === 'OPTIONS') {
+            res.status(200).end();
+        } else {
+            next();
+        }
+    });
+
+    app.use('/', routes);
+
+    var server = app.listen(3001, function() {
+        console.log('Express server listening on port ' + server.address().port);
+    });
+
+    module.exports = app;
+}());
