c534e7bc588c075b79cad5b63306e06aec8cff4a
Praful Rana <prafulrana@gmail.com>
2015-05-27 17:21:24 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@
     app.use(bodyParser.json());
 
     app.all('*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://web-staging-runnabledemo.runnable2.net:3000");
+        res.header("Access-Control-Allow-Origin", "http://web-staging-runnabledemo.runnableapp.com:3000);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
         res.header("Access-Control-Allow-Credentials", "true");
