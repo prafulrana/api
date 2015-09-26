5137dc77069e961514260c55a59a5c104a5fde85
Praful Rana <prafulrana@gmail.com>
2015-09-25 17:45:19 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,9 +15,9 @@
 
     app.all('*', function(req, res, next) {
         // res.header("Access-Control-Allow-Origin", process.env.WEB_CLIENT_HOSTNAME);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
-        res.header("Access-Control-Allow-Credentials", "true");
+        // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        // res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
+        // res.header("Access-Control-Allow-Credentials", "true");
         if (req.method === 'OPTIONS') {
             res.status(200).end();
         } else {
